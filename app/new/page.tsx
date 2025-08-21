b5aebf80c09835a8e7ed5f5b1dec7340362ae237
Angelo <angeloabihanna19@gmail.com>
2025-08-22 01:41:16 +0200

    fix commit

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,18 +1,14 @@
 import { prisma } from "@/db";
-import { redirect } from "next/navigation";
 import Link from "next/link";
 
 async function createTodo(data: FormData) {
 "use server";
-
 const title = data.get("title")?.valueOf()
 if (typeof title !== "string" || title.length === 0) {
     throw new Error("Invalid Title")
 }
 
 await prisma.todo.create({ data: { title, complete: false }})
-
-redirect("/")
 }
 
 const page = () => {
